38c4f69a387eb1013e04722d27386aee0244ca70
ghxs1 <ewomokaro@gmail.com>
2025-03-10 14:46:21 -0500

    Updated terraform config files

--- network.tf
+++ network.tf
@@ -11,6 +11,7 @@ resource "aws_security_group" "example_security_group" {
     name = "example_security_group"
     vpc_id = "${aws_vpc.example_vpc.id}"
     description = "Security group for example_vpc"
+    
 }
 
 resource "aws_vpc_security_group_ingress_rule" "example_security_group_ingress_http" {
@@ -19,14 +20,14 @@ resource "aws_vpc_security_group_ingress_rule" "example_security_group_ingress_h
     ip_protocol = "tcp"
     from_port = 80
     to_port = 80
-    cidr_ipv4 = "${aws_subnet.example_vpc.cidr_block}"
+    cidr_ipv4 = "${aws_subnet.example_vpc_subnet.cidr_block}"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "example_security_group_ingress_ssh" {
     security_group_id = "${aws_security_group.example_security_group.id}"
-    
+
     ip_protocol = "tcp"
     from_port = 22
     to_port = 22
-    cidr_ipv4 = "${aws_subnet.example_vpc.cidr_block}"
+    cidr_ipv4 = "${aws_subnet.example_vpc_subnet.cidr_block}"
 }
