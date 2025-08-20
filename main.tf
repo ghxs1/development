a7f60af11c4e423dbe682d6078de615f95a18b7c
ghxs1 <ewomokaro@gmail.com>
2025-08-20 13:26:26 -0500

    Updated public key file location

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "rhel9_server" { 
     key_name = "rhel9_server"
-    public_key = file("/Users/local-user/.ssh/webserver.pub")
+    public_key = file("/Users/eghosa/.ssh/webserver.pub")
   
 }
